d4c06cc06d4a360b801242074fb92d39670e6f9d
Abhinav_Kashyap <kashyap06abhinav@gmail.com>
2025-06-22 23:59:44 +0530

    progress in Testing framework

--- Testing_Framework/Test.py
+++ Testing_Framework/Test.py
@@ -15,45 +15,58 @@ full_path = os.path.join(script_dir, "Test_Data.csv")
 data = pandas.read_csv(full_path, header = 0)
 S_No = data['S.No'].values
 Fun_name = data['Function_Name'].values
-param_data = {}
+Param_dict = {}
 for col in data.columns:
     if 'Param' in col:
-        param_data[col] = list(data[col].values)
+        Param_dict[col] = list(data[col].values)
     else:
         pass
 Exp_result = data['Expected_Result'].values
-Testing_script_name = data['Test_Sript'].values
+Testing_script_name = data['Test_Script'].values
 Testing_class_name = data['Class_Name'].values
 
-if param_data:
-    print(param_data)
-else:
-    print("none")
+for i in S_No:
+    if Param_dict:
+        tup = (S_No[i-1], Fun_name[i-1])
+        for key in Param_dict.keys():
+            tup = tup + (Param_dict[key][i-1],)
+        tup = tup + (Exp_result[i-1],)
+        parameters.append([tup])
+    else:
+        parameters.append([(S_No[i-1], Fun_name[i-1], Exp_result[i-1])])
 
-@pytest.fixture
-def mockios():
-    #mocking argument parser for HC script
-    test_args = [script_name]
-    with mock.patch.object(sys, 'argv', test_args):
-        test_script = importlib.import_module(script_name)
-        imported_class = getattr(test_script, class_name)
+# @pytest.fixture
+# def mockios():
+#     #mocking argument parser for HC script
+#     test_args = [Testing_script_name[0]]
+#     with mock.patch.object(sys, 'argv', test_args):
+#         test_script = importlib.import_module(Testing_script_name[0])
+#         if Testing_class_name:
+#             imported_class = getattr(test_script, Testing_class_name[0])
+#         else:
+#             pass
     
-    #patching the constructor to do nothing
-    with patch.object(imported_class, '_init_', return_value=None):
-        obj = imported_class("tech_spec_file.xlsx", generate_file=False)
-        #Manually set required attributes
-        obj.Debug = False
-        obj.wbkName = "tech_spec_file.xlsx"
-        obj.add_ecm = MagicMock()
-        return obj
+#     #patching the constructor to do nothing
+#     with patch.object(imported_class, '_init_', return_value=None):
+#         obj = imported_class()
+#         #Manually set required attributes
+#         obj = MagicMock()
+#         return obj
+
+decorater_parameter_names = ""
+for col in data:
+    if "Test_Script" not in col and "Class_Name" not in col:
+        decorater_parameter_names+=','+col
+    else:
+        pass
     
-# Calling all paramaters for testing
-@pytest.mark.parametrize('sno, param, filters, test_output, result', parameters)
-def test_policy(mock_ios, sno, param, filters, test_output, result):
-    return_result = getattr(mock_ios, param) (param, filters, test_output)
-    assert return_result == True
-    mock_ios.add_ecm.assert_called_once()
-    called_param, called_result = mock_ios.add_ecm.call_args[0]
-    assert called_param == param
-    assert result.upper() in called_result.upper()
+# print(parameters[-1][])
+
+# # calling all paramaters for testing
+# @pytest.mark.parametrize(getattr(decorater_parameter_names), parameters)
+# def test_policy(sno, function, param, test_output, result):
+#     return_result = getattr(mock_ios, param) (param, filters, test_output)
+#     assert return_result == parameters[-1]
+#     # mock_ios.add_ecm.assert_called_once()
+    # called_param, called_result = mock_ios.add_ecm.call_args[0]
          
